179b96ad767d118ac65249cc518d24930c620f75
Trang Pham <trpham@c1r1p9.hive.fi>
2025-03-11 14:09:08 +0200

    Add send_signal function - Handle error when kill function fail - Make use of send_signal for both client and server

--- client.c
+++ client.c
@@ -6,21 +6,20 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 11:39:13 by trpham            #+#    #+#             */
-/*   Updated: 2025/03/10 14:56:46 by trpham           ###   ########.fr       */
+/*   Updated: 2025/03/11 14:08:42 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int validate_PID(char *arg);
-
-static void	send_signal(int	pid, unsigned char c);
-static void action(int signal);
+static void	send_message(int pid, char *message);
+static int validate_and_return_PID(char *arg);
+// static void action(int signal);
 
 int	main(int ac, char *av[])
 {
 	char	*msg;
-	int		i;
+	// int		i;
 	int		PID;
 
 	if (ac != 3)
@@ -28,80 +27,64 @@ int	main(int ac, char *av[])
 		ft_putstr_fd("Please provide PID and message\n", 1);
 		exit(EXIT_FAILURE);
 	}
-	PID = validate_PID(av[1]);
+	PID = validate_and_return_PID(av[1]);
 	if (PID == -1)
 		exit(EXIT_FAILURE);
-	
-	signal(SIGUSR1, action);
-	signal(SIGUSR2, action);
-
 	msg = av[2];
-	i = 0;
-	while (msg[i])
-	{
-		send_signal(PID, msg[i]);	
-		i++;
-	}
 	
+	// signal(SIGUSR1, action);
+	// signal(SIGUSR2, action);
+	send_signal(PID, SIGUSR1); //signal start of communication
+	send_message(PID, msg);
+	// i = 0;
+	// while (msg[i])
+	// {
+	// 	send_signal(PID, msg[i]);	
+	// 	i++;
+	// }
+	// kill(PID, '\0');
 	return (0);
 }
 
-static void	send_signal(int	pid, unsigned char c)
+static void	send_message(int pid, char *message)
 {
-	int				i;
+	int	i;
+	unsigned char	c; //1 unsign char is 8 bits to write, sign char has 1 sign bit
 
-	i = 7;
-	while (i >= 0)
+	while (*message)
 	{
-		// while (!ready_to_receive)
-		// 	pause();
-		if ((c & (1 << i)) == 0)
+		c = *message;
+		i = 7;
+		while (i >= 0)
 		{
-			kill(pid, SIGUSR2); // 1 = 00000001, bitmask isolating the ith bit in c
-		} 
-		else
-		{
-			kill(pid, SIGUSR1);
+			if ((c & (1 << i)) == 0)
+			{
+				send_signal(pid, SIGUSR2);
+				// kill(pid, SIGUSR2); // 1 = 00000001, bitmask isolating the ith bit in c
+			} 
+			else
+			{
+				send_signal(pid, SIGUSR1);
+				// kill(pid, SIGUSR1);
+			}
+			i--;
+			usleep(100); 
 		}
-		i--;
-		usleep(100); 
+		message++;
 	}
-	while (1)
-	{
-		pause();
-	}
-	
-	// write(1, "\n", 1);
+	send_signal(pid, SIGUSR2); //sending termination signal 0
 }
 
-static void action(int signal)
-{
-	// static int	received;
-
-	if (signal == SIGUSR1)
-		// printf("receive %d/n", SIGUSR1);
-		kill(getpid(), SIGUSR1);
-
-	else
-	{
-		// printf("receive %d/n", SIGUSR2);
-		kill(getpid(), SIGUSR2);
-
-		// ft_putnbr_fd(received, 1);
-		// exit(0);
-	}
-}
-
-static int validate_PID(char *arg)
+static int validate_and_return_PID(char *arg)
 {
 	int	PID;
 	int	i;
 
-	if (ft_strlen(arg) != 6)
-	{
-		ft_putstr_fd("Not a valid PID\n", 1);
-		return (-1);
-	}
+	// if (ft_strlen(arg) != 6)
+	// {
+	// 	ft_putstr_fd("Not a valid PID\n", 1);
+	// 	return (-1);
+	// }
 	i = 0;
 	while (arg[i])
 	{
@@ -120,6 +103,25 @@ static int validate_PID(char *arg)
 	}
 	return (PID);
 }
+
+// static void action(int signal)
+// {
+// 	// static int	received;
+
+// 	if (signal == SIGUSR2)
+// 		printf("not ready to receive /n");
+// 		// kill(getpid(), SIGUSR1);
+
+// 	else
+// 	{
+// 		printf("ready to receive /n");
+// 		// kill(getpid(), SIGUSR2);
+
+// 		// ft_putnbr_fd(received, 1);
+// 		// exit(0);
+// 	}
+// }
+
 
 /* 
 Potential problem with signal transmission
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:15:12 by trpham            #+#    #+#             */
-/*   Updated: 2025/03/10 14:44:10 by trpham           ###   ########.fr       */
+/*   Updated: 2025/03/11 14:03:52 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,17 @@ int	main()
 	int	pid;
 
 	pid =  getpid();
+	ft_putstr_fd("PID :", 1);
 	ft_putnbr_fd(pid, 1);
 	ft_putstr_fd("\n", 1);
+	
 	set_signal_action();
+	// send_signal()
 	while (1)
 	{
-		if (!ready_to_receive)
+		// if (!ready_to_receive)
 			pause();
-		ready_to_receive = 0;
+		// ready_to_receive = 0;
 	}
 	return (0);
 }
@@ -40,14 +43,16 @@ static void	set_signal_action(void)
 	struct sigaction act;
 	
 	// ft_bzero(&act, sizeof(act));
-	act.sa_sigaction = &signal_handler;
 	sigemptyset(&act.sa_mask);
 	act.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &act, NULL) == -1)
-		printf("Sigaction failed to receive signal\n"); //sigaction return 0 on success and -1 on error
-	if (sigaction(SIGUSR2, &act, NULL) == -1)
-		printf("Sigaction failed to receive signal\n");
-	sigaction(SIGTERM, &act, NULL);
+	act.sa_sigaction = &signal_handler;
+	if (sigaction(SIGUSR1, &act, NULL) == -1
+		|| sigaction(SIGUSR2, &act, NULL) == -1)
+	{
+		ft_putstr_fd("Sigaction failed \n", 1); //sigaction return 0 on success and -1 on error
+		exit(EXIT_FAILURE);
+	}
+	// sigaction(SIGTERM, &act, NULL);
 }
 
 static void	signal_handler(int signal, siginfo_t *info, void *context)
@@ -59,47 +64,60 @@ static void	signal_handler(int signal, siginfo_t *info, void *context)
 	
 	(void)context;
 	// (void)info;
+	// ready_to_receive = 1;
 	temp_c = temp_c << 1;
-	if (signal == SIGTERM)
-	{
-		ft_putstr_fd("Transmission completed\n", 1);
-		exit(EXIT_SUCCESS);
-	}
+	// if (signal == SIGTERM)
+	// {
+	// 	ft_putstr_fd("Transmission completed\n", 1);
+	// 	exit(EXIT_SUCCESS);
+	// }
 	if (info->si_pid)
 		client_id = info->si_pid;
+
+	if (!ready_to_receive)
+	{
+		if (signal == SIGUSR1) // receive sigusr1 from client, ready to communicate
+		{
+			send_signal(client_id, SIGUSR2); // server acknowledge and send to client, ready to receive
+			ready_to_receive = 1;
+		}
+		return ;
+	}
 
 	if (signal == SIGUSR1)
 	{
 		// ft_putstr_fd("Receive bit 1 \n", 1);
 		temp_c = temp_c | 1;
 		// printf("%d\n", info->si_pid);
-		kill(client_id, SIGUSR1);
+		// kill(client_id, SIGUSR1);
 	}
-	else if (signal == SIGUSR2)
-	{
-		// ft_putstr_fd("Receive bit 0 \n", 1);
-		kill(client_id, SIGUSR2);
-		// printf("%d\n", info->si_pid);
-	}
+	// else if (signal == SIGUSR2)
+	// {
+	// 	// ft_putstr_fd("Receive bit 0 \n", 1);
+	// 	kill(client_id, SIGUSR2);
+	// 	// printf("%d\n", info->si_pid);
+	// }
 	
 	i++;
 	if (i == 8)
 	{
-		// if (temp_c == '\0')
-		// {
-		// 	ft_putstr_fd("\n", 1);
-		// 	ft_putstr_fd("Transmission completed\n", 1);
-		// 	// exit(EXIT_SUCCESS);
-		// }
+		if (temp_c == '\0')
+		{
+			// ft_putstr_fd("\n", 1);
+			send_signal(client_id, SIGUSR2); // termination signal
+			ft_putstr_fd("Transmission completed\n", 1);
+			// ready_to_receive = 0;
+			exit(EXIT_SUCCESS);
+		}
 		ft_putchar_fd(temp_c, 1);
 		temp_c = 0;
 		i = 0;
-		ready_to_receive = 1;
 	}
+	send_signal(client_id, SIGUSR1); //acknowledge each received bit
+	// kill(client_id, SIGUSR1);
 	// send signal to client to acknowlege signal receipt for each bit
 	// if (signal == SIGUSR1)
 	// 	kill(info->si_pid, SIGUSR1);
 	// else if (signal == SIGUSR2)
 	// 	kill(info->si_pid, SIGUSR2);
-	// ready_to_receive = TRUE;
 }
